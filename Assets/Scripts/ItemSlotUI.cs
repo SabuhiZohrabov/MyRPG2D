3d12a776159e19e5de93739ecead8a1b55a7a2ee
SabuhiZohrabov <111567994+SabuhiZohrabov@users.noreply.github.com>
2025-09-04 13:35:28 +0400

    item equip hazırdı

--- Assets/Scripts/ItemSlotUI.cs
+++ Assets/Scripts/ItemSlotUI.cs
@@ -1,6 +1,7 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using UnityEngine.EventSystems;
 
 public class ItemSlotUI : MonoBehaviour
 {
@@ -9,12 +10,78 @@ public class ItemSlotUI : MonoBehaviour
     public TMP_Text amountText;
     public Image isEquipedBG;
 
+    private ItemSO currentItem;
+    private Button button;
+
+    private void Awake()
+    {
+        // Try to get Button component first
+        button = GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(OnItemClicked);
+        }
+        else
+        {
+            // If no Button, add click detection to iconImage (ItemIcon)
+            if (iconImage != null)
+            {
+                iconImage.raycastTarget = true;
+                
+                // Add EventTrigger for click detection
+                var eventTrigger = iconImage.gameObject.GetComponent<UnityEngine.EventSystems.EventTrigger>();
+                if (eventTrigger == null)
+                {
+                    eventTrigger = iconImage.gameObject.AddComponent<UnityEngine.EventSystems.EventTrigger>();
+                }
+
+                // Add click event
+                var clickEntry = new UnityEngine.EventSystems.EventTrigger.Entry();
+                clickEntry.eventID = UnityEngine.EventSystems.EventTriggerType.PointerClick;
+                clickEntry.callback.AddListener((data) => OnItemClicked());
+                eventTrigger.triggers.Add(clickEntry);
+            }
+        }
+    }
+
     public void Setup(ItemSO item, int amount, bool isEquiped)
     {
         if (!Application.isPlaying) return;
+        
+        currentItem = item;
         iconImage.sprite = item.icon;
         nameText.text = item.displayName;
         amountText.text = amount.ToString();
         isEquipedBG.gameObject.SetActive(isEquiped);
+    }
+
+    private void OnItemClicked()
+    {
+        if (currentItem == null) return;
+
+        bool success = false;
+        bool wasEquipped = InventoryManager.Instance.IsItemEquipped(currentItem.itemId);
+
+        if (wasEquipped)
+        {
+            success = InventoryManager.Instance.UnequipItem(currentItem.itemId);
+        }
+        else
+        {
+            success = InventoryManager.Instance.EquipItem(currentItem.itemId);
+        }
+
+        if (success)
+        {
+            FindFirstObjectByType<InventoryPanelUI>().RefreshUI();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (button != null)
+        {
+            button.onClick.RemoveListener(OnItemClicked);
+        }
     }
 }
